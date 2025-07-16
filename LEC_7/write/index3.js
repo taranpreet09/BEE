2fbe801ac6504f7e5cc6312bf3108757b2e06920
taranpreet09 <kaur.taran223@gmail.com>
2025-07-16 15:20:43 +0530

    final

--- LEC_7/write/index3.js
+++ LEC_7/write/index3.js
@@ -1,19 +1,18 @@
 const fs = require("fs");
 
-fs.readFile("./LEC_7/users.txt", "utf-8", function (err, data1) {
+fs.readFile("./LEC_7/users.txt", "utf-8", function (err, data) {
     if (err) return console.log(err);
-    let users= JSON.parse(data1);
-    console.log(users);  
+    let users1= JSON.parse(data);
+    console.log(users1);  
 
-fs.readFile("./LEC_7/users2.txt", "utf-8", function (err, data2) {
+fs.readFile("./LEC_7/users2.txt", "utf-8", function (err, data) {
     if (err) return console.log(err);
-    let users= JSON.parse(data1);
-    console.log(users);  
+    let users2= JSON.parse(data);
+    console.log(users2);  
 
+let allusers= users1.concat(users2)  // const combined data = data1+ data2
 
-const combinedData = data1 + data2;
-
-fs.writeFile("./LEC_7/users3.txt",combinedData,function(err){
+fs.writeFile("./LEC_7/users3.txt",JSON.stringify(allusers),function(err,data){
     if(err) return console.log(err);
     console.log("Users data written to file successfully");
 });
